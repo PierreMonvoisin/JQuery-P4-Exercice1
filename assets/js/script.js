d7b48d69c13912eba8b894787154c021b5a80dc5
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-11-07 16:49:18 +0100

    Exercice terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,37 @@
 $(function() {
   $('img').css({'width' : '150px','display' : 'none'});
   $('a').mouseover(function(){$(this).css('cursor', 'pointer')});
+  var choice = '';
+  var iaChoice = '';
+  var result = '';
 
   // Affiche votre choix Pierre / Feuille / Ciseaux
   $('a').click(function(){
-    var choice = $(this).attr('id');
+    choice = $(this).attr('id');
     if ($('.person img').css('display', 'block')){
       $('.person img').css('display', "none")
     };
+    $('.robot img').css('display', "none")
     $('.'+choice).css('display', 'block');
+    $('label h1').text("Let's battle ! Appuyez sur la barre d'espace").css('color','green');
+    console.log(choice);
   });
-
   // Lance le choix de l'IA
   $(document).keydown(function(e){
     if (e.keyCode == 32 ){
       if ($('.person img').is(':visible')){
-        $('label h1').text("Let's battle !").css('color','green');
         IAchoice();
-        // Results();
+        Results();
       } else {
         $('label h1').text('Faites votre choix :').css('color','red');
       };
     };
+
   });
 
   //Genère un nombre aléatoire et l'associe à Pierre / Feuille / Ciseaux et affiche le résultat
   function IAchoice(){
-    var iaChoice = Math.floor(Math.random() * 3) + 1;
+    iaChoice = Math.floor(Math.random() * 3) + 1;
     if (iaChoice == 1){
       iaChoice = "IArock";
     } else if (iaChoice == 2){
@@ -38,6 +43,33 @@ $(function() {
       $('.robot img').css('display', "none")
     };
     $('.'+iaChoice).css('display', 'block');
-    console.log(iaChoice)
+    console.log(iaChoice);
+  };
+
+  //Vérification du gagnant
+  function Results(){
+    if (('IA' + choice) == iaChoice){
+      result="Egalité";
+      $('label h1').text(result).css('color','blue');
+    } else if (choice == "rock" && iaChoice == "IAscissors"){
+      result="Gagné !";
+      $('label h1').text(result).css('color','green');
+    } else if (choice == "paper" && iaChoice == "IArock"){
+      result="Gagné !";
+      $('label h1').text(result).css('color','green');
+    } else if (choice == "scissors" && iaChoice == "IApaper"){
+      result="Gagné !";
+      $('label h1').text(result).css('color','green');
+    } else if (iaChoice == "IArock" && choice == "scissors"){
+      result="Perdu !";
+      $('label h1').text(result).css('color','red');
+    } else if (iaChoice == "IApaper" && choice == "rock"){
+      result="Perdu !";
+      $('label h1').text(result).css('color','red');
+    } else if (iaChoice == "IAscissors" && choice == "paper"){
+      result="Perdu !";
+      $('label h1').text(result).css('color','red');
+    }
+    console.log(result);
   };
 });
